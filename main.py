77fa9e4f8b287d67808831fc6e68541898b7658e
MunirAmer <moneer2004000@gmail.com>
2022-12-14 19:14:44 +0300

    done

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-
 # write your code here
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    
+
+    def is_adult(self):
+        if self.age >= 18:
+            print("You have too much responsibilities")
+        else:
+            print("Lucky you")    
+
+    def __str__(self):
+        return f"My name is {self.name} and I am {self.age} years old"
+
+
+first_person = Person("Munir", 18)
+print(first_person.name)
+print(first_person.age)
+first_person.is_adult()
+print(first_person)
+
